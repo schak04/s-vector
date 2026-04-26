7b4c5a39d8957bb3184adbc8de026e04233dba3e
Saptaparno Chakraborty <schakraborty1086@gmail.com>
2026-04-26 23:31:47 +0530

    feat: make initial struct

---
+++ main.c
@@ -0,0 +1,19 @@
+/*
+s-vector
+Author: Saptaparno Chakraborty
+*/
+
+#include <stdio.h>
+
+// components: data (ptr) + size + capacity
+struct s_vector {
+    int* data; // changes whenever the array needs resizing
+    size_t size;
+    size_t capacity;
+};
+
+// too sleepy rn
+
+int main() {
+    return 0;
+}
