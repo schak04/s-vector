55130278eab9f3f97e655ad2453ac3767789f49e
Saptaparno Chakraborty <schakraborty1086@gmail.com>
2026-04-27 21:16:52 +0530

    fix: init function

--- main.c
+++ main.c
@@ -14,18 +14,19 @@ struct s_vector {
     size_t capacity;
 };
 
+/*
+design decision for s_vector's cap and size:
+    initially size -> 0
+    capacity -> 5
+every time,
+    until size == capacity, capacity doesn't change.
+    as soon as size == capacity -> cap = size + 5 again.
+*/
+
 void init(struct s_vector* vec) {
-    vec->data = NULL;
     vec->size = INITIAL_SIZE;
-    vec->capacity = vec->size + 5;
-    /*
-    design decision for s_vector's cap and size:
-        initially size -> 0
-        capacity -> 5
-    every time,
-        until size == capacity, capacity doesn't change.
-        as soon as size == capacity -> cap = size + 5 again.
-    */
+    vec->capacity = 5;
+    vec->data = (int*)malloc(vec->capacity * sizeof(int));
 }
 
 int main() {
