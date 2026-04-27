94c28feed1ddbeb8db31d7da5d322c9d1c95b14d
Saptaparno Chakraborty <schakraborty1086@gmail.com>
2026-04-27 14:19:43 +0530

    feat: init function

--- main.c
+++ main.c
@@ -4,6 +4,7 @@ Author: Saptaparno Chakraborty
 */
 
 #include <stdio.h>
+#define INITIAL_SIZE 0
 
 // components: data (ptr) + size + capacity
 struct s_vector {
@@ -12,7 +13,19 @@ struct s_vector {
     size_t capacity;
 };
 
-// too sleepy rn
+void init(struct s_vector* vec) {
+    vec->data = NULL;
+    vec->size = INITIAL_SIZE;
+    vec->capacity = vec->size + 5;
+    /*
+    design decision for s_vector's cap and size:
+        initially size -> 0
+        capacity -> 5
+    every time,
+        until size == capacity, capacity doesn't change.
+        as soon as size == capacity -> cap = size + 5 again.
+    */
+}
 
 int main() {
     return 0;
