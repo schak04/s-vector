5c83ffd9b2e4c0fac6aad8fa090e34588cd2fad9
Saptaparno Chakraborty <schakraborty1086@gmail.com>
2026-05-06 23:52:55 +0530

    feat: implement insertion at the end and deallocation FINALLY debugged a segfault and learned a bit of GDB too for the future for actual systems programming

--- main.c
+++ main.c
@@ -29,6 +29,35 @@ void init(struct s_vector* vec) {
     vec->data = (int*)malloc(vec->capacity * sizeof(int));
 }
 
+void free_s_vector(struct s_vector* vec) {
+    vec->size = 0;
+    vec->capacity = 0;
+    free(vec);
+}
+
+void s_push_back(struct s_vector* vec, int val) {
+    vec->size += 1;
+    if (vec->size >= vec->capacity) {
+        vec->capacity = vec->size + 5;
+    }
+    /*
+    vec->size at index 0 = 1
+    vec->size at index x = x+1
+    so while pushing back,
+    idx = vec->size - 1 
+    */
+    *(vec->data + (vec->size - 1)) = val;
+}
+
 int main() {
+    struct s_vector* v = malloc(sizeof(struct s_vector));
+    init(v);
+
+    s_push_back(v, 4);
+
+    printf("%d\n", v->data[0]);
+
+    free_s_vector(v);
+ 
     return 0;
 }
